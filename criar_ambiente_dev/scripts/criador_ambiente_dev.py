04f4057f59fe3e3412f3741799a0399cfd276ecd
Maria Eduarda <mariaeducorrea2502@gmail.com>
2025-08-04 22:02:57 -0300

    Enviando alterações no projeto.

--- criar_ambiente_dev/scripts/criador_ambiente_dev.py
+++ criar_ambiente_dev/scripts/criador_ambiente_dev.py
@@ -3,7 +3,7 @@ import subprocess
 import shutil
 
 
-print("Vamos iniciar a criação do projeto...")
+print("Iniciando projeto.")
 
 project_name = input("Nome do projeto: ")
 project_path = input("Local do projeto: ")
@@ -17,12 +17,58 @@ project_path = project_path.replace("\\", "/")
 #Usar os para concatenar caminho passado + nome do projeto que é o diretório criado (BR> full_path = caminho completo)
 full_path = os.path.join(project_path, project_name)
 
-#Executar comando terminal para criar projeto com poetry
+#Executar comando terminal para criar projeto com poetry / cwd é onde será executado o comando 
 subprocess.run(["poetry", "new", project_name], cwd=project_path)
 
 subprocess.run(["poetry", "env", "use", f"python{version_python}"], cwd=full_path)
 
-subprocess.run(["poetry", "install"], cwd=full_path)
+criando_gitignore = """
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*$py.class
+
+# Poetry virtual environments
+.venv/
+
+# Distribution / packaging
+.Python
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+*.egg-info/
+.installed.cfg
+*.egg
+
+# Logs and local config
+*.log
+*.env
+*.ini
+*.local
+.env.*
+.vscode/
+
+# VS Code settings
+.vscode/
+
+# macOS
+.DS_Store
+
+# Windows
+Thumbs.db
+ehthumbs.db
+"""
+
+with open(os.path.join(full_path, ".gitignore"), "w") as f:
+    f.write(criando_gitignore.strip())
 
 #Strip remove espaços no começo e final caso tenha e com split deixa somente palavras
 if packages.strip():
@@ -35,7 +81,7 @@ else:
 if shutil.which("code"):
     subprocess.run("code .", cwd=full_path, shell=True)
 else:
-    print("VS Code não está disponível no terminal. Pulei essa parte.")
+    print("VS Code não está disponível.")
 
 
 # Impede que o terminal feche imediatamente (opcional)
